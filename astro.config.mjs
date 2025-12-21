614c71f62637d1042265da47982d4f0c7eabf797
san-ren <3298065189@qq.com>
2025-12-21 08:33:10 +0800

    配置 Sveltia CMS 并清理 Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import keystatic from '@keystatic/astro';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), keystatic()],
-  
-  // 1. 你的 GitHub Pages 完整域名
+  integrations: [tailwind(), react()], // 里面只有这两个
   site: 'https://san-ren.github.io',
-  
-  // 2. 关键！必须加上你的仓库名作为后缀
-  base: '/my-nav', 
+  base: '/my-nav',
 });
