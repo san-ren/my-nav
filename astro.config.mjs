64dfa203d187cf0b911f015c73e3aa15909f544c
san-ren <3298065189@qq.com>
2025-12-23 10:46:49 +0800

    图标获取、缓存 回退

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,13 @@ export default defineConfig({
   // 添加这段配置来关闭底部工具栏
   devToolbar: {
     enabled: false
+  },
+    vite: {
+    server: {
+      watch: {
+        usePolling: true,   // 强制使用轮询机制，解决 Windows 文件系统通知丢失的问题
+        interval: 1000,     // 每 1000 毫秒检查一次变动（嫌慢可以改成 500）
+      }
+    }
   }
 });
