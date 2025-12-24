843d4881aeee34c602307a89b79aad9f7ff72178
san-ren <3298065189@qq.com>
2025-12-24 22:14:22 +0800

    优化移动端体验

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,22 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()], // 里面只有这两个
+  integrations: [tailwind(), react()],
   site: 'https://san-ren.github.io',
   base: '/my-nav',
-  // 添加这段配置来关闭底部工具栏
+  devOptions: {
+    host: '0.0.0.0',
+    port: 4321,
+  },
+  // 这些配置项应该在顶层，而不是在devOptions内部
   devToolbar: {
     enabled: false
   },
-    vite: {
+  vite: {
     server: {
       watch: {
-        usePolling: true,   // 强制使用轮询机制，解决 Windows 文件系统通知丢失的问题
-        interval: 1000,     // 每 1000 毫秒检查一次变动（嫌慢可以改成 500）
+        usePolling: true,
+        interval: 1000,
       }
     }
   }
